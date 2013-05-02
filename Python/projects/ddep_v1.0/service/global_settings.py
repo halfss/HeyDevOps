a3ceeffbe404e84d67da1f260ff073866a103a2b
mcsrainbow <guosuiyu@gmail.com>
2013-05-02 11:47:30 +0000

    Update auth.ini and global_settings.py

--- Python/projects/ddep_v1.0/service/global_settings.py
+++ Python/projects/ddep_v1.0/service/global_settings.py
@@ -5,5 +5,5 @@
 # Date: Fri 29 Mar 2013 10:37:48 AM CST
 # Author: Dong Guo
 
-PROJECT_HOME = "/home/dong.guo/github/HeyDevOps/Python/project/ddep_v1.0"
-CONFIG_HOME = "/home/dong.guo/github/HeyDevOps/Python/project/ddep_v1.0/conf"
+PROJECT_HOME = "/home/dong.guo/github/HeyDevOps/Python/projects/ddep_v1.0"
+CONFIG_HOME = "/home/dong.guo/github/HeyDevOps/Python/projects/ddep_v1.0/conf"
