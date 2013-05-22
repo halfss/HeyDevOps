2c6aee853993268b83efce4d2dc7e9ac13de165b
Administrator <admin@local.host>
2013-05-23 00:36:53 +0800

    Remove ddep_v1.0

--- Python/projects/ddep_v1.0/service/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-#-*- coding:utf-8 -*-
-
-# FileName: __init.py
-# Date: Sat 30 Mar 2013 07:28:42 PM CST
-# Author: Dong Guo
-
-from config import dbconf
--- Python/projects/ddep_v1.0/service/global_settings.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-#-*- coding:utf-8 -*-
-
-# FileName: global_settings.py
-# Date: Fri 29 Mar 2013 10:37:48 AM CST
-# Author: Dong Guo
-
-PROJECT_HOME = "/home/dong.guo/github/HeyDevOps/Python/projects/ddep_v1.0"
-CONFIG_HOME = "/home/dong.guo/github/HeyDevOps/Python/projects/ddep_v1.0/conf"
